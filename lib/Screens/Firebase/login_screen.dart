3b2a0d9f90b924670f7452eb45aa419d5a7520d1
ShahzadUmarBaig <ShahzadUBaig@gmail.com>
2020-11-08 11:15:22 +0500

    Added Functionality to these screens
    Signed-off-by: ShahzadUmarBaig <ShahzadUBaig@gmail.com>

--- lib/Screens/Firebase/login_screen.dart
+++ lib/Screens/Firebase/login_screen.dart
@@ -1,8 +1,12 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:medkube/Screens/Firebase/register_screen.dart';
 import 'package:medkube/Widgets/custom_button.dart';
 import 'package:medkube/Widgets/custom_textfield.dart';
 import 'package:medkube/constants.dart';
+
+import '../../extras.dart';
 
 class LoginScreen extends StatefulWidget {
   static String id = "LoginScreen";
@@ -23,46 +27,56 @@ class _LoginScreenState extends State<LoginScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
+    return Material(
+      child: Container(
         height: double.infinity,
         color: Colors.white,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image(
-              image: AssetImage('images/icons8_login_200px_1.png'),
-            ),
-            CustomTextField(
-              labelText: "Username",
-              textEditingController: usernameController,
-            ),
-            CustomTextField(
-                labelText: "Password",
-                textEditingController: passwordController,
-                obscureText: true),
-            CustomButton(
-              buttonColor: Colors.lightBlueAccent,
-              buttonText: "Login",
-              buttonTextColor: Colors.white,
-            ),
-            RichText(
-              text: TextSpan(
-                style: kDefaultStyle,
+        child: ScrollConfiguration(
+          behavior: MyBehavior(),
+          child: ListView(
+            padding:
+                EdgeInsets.only(top: MediaQuery.of(context).size.height / 7),
+            children: [
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  TextSpan(text: 'Not a regular user? '),
-                  TextSpan(
-                    text: 'Register!',
-                    style: kLinkStyle,
-                    recognizer: TapGestureRecognizer()
-                      ..onTap = () {
-                        Navigator.pushNamed(context, LoginScreen.id);
-                      },
+                  Image(
+                    image: AssetImage('images/icons8_login_200px_1.png'),
+                  ),
+                  CustomTextField(
+                    labelText: "Username",
+                    textEditingController: usernameController,
+                  ),
+                  CustomTextField(
+                      labelText: "Password",
+                      textEditingController: passwordController,
+                      obscureText: true),
+                  CustomButton(
+                    buttonColor: Colors.lightBlueAccent,
+                    buttonText: "Login",
+                    buttonTextColor: Colors.white,
+                  ),
+                  RichText(
+                    text: TextSpan(
+                      style: kDefaultStyle,
+                      children: [
+                        TextSpan(text: 'Not a regular customer? '),
+                        TextSpan(
+                          text: 'Become one!',
+                          style: kLinkStyle,
+                          recognizer: TapGestureRecognizer()
+                            ..onTap = () {
+                              Navigator.pushNamed(
+                                  context, RegistrationScreen.id);
+                            },
+                        ),
+                      ],
+                    ),
                   ),
                 ],
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
