1a46152cbfdf3a299d879b5b349a53da4b3238ff
ShahzadUmarBaig <ShahzadUBaig@gmail.com>
2020-11-22 21:18:21 +0500

    fixed order screen font
    Signed-off-by: ShahzadUmarBaig <ShahzadUBaig@gmail.com>

--- lib/Widgets/cart_item_tile.dart
+++ lib/Widgets/cart_item_tile.dart
@@ -52,6 +52,7 @@ class CartItemTile extends StatelessWidget {
               ),
             ),
             Expanded(
+              flex:2,
               child: Container(
                 margin: EdgeInsets.symmetric(vertical: 8.0),
                 child: MaterialButton(
@@ -81,33 +82,30 @@ class CartItemTile extends StatelessWidget {
                 ),
               ),
             ),
-            Container(
-              height: 50,
-              width: 85,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text("Total",
-                      style: GoogleFonts.montserrat(
-                          fontSize: 14.0, color: Colors.black54)),
-                  SizedBox(height: 2.0),
-                  Text(price.toString(),
-                      style: GoogleFonts.montserrat(
-                          fontSize: 16.0,
+            VerticalDivider(endIndent: 12,indent: 12,color: Colors.black54,thickness: 0,width: 0,),
+            Expanded(
+              flex:1,
+              child: Container(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text("Total",
+                        style: GoogleFonts.montserrat(
+                            fontSize: 14.0, color: Colors.black54)),
+                    SizedBox(height: 2.0),
+                    Text(price.toString(),
+                        style: GoogleFonts.montserrat(
+                            fontSize: 16.0,
+                            color: Colors.black54,
+                            fontWeight: FontWeight.w500)),
+                    Text("PKR",
+                        style: GoogleFonts.montserrat(
+                          fontSize: 14.0,
                           color: Colors.black54,
-                          fontWeight: FontWeight.w500)),
-                  Text("PKR",
-                      style: GoogleFonts.montserrat(
-                        fontSize: 14.0,
-                        color: Colors.black54,
-                      )),
-                ],
-              ),
-              decoration: BoxDecoration(
-                border: Border(
-                  left: BorderSide(
-                      color: Colors.grey[300], style: BorderStyle.solid),
+                        )),
+                  ],
                 ),
+
               ),
             ),
           ],
--- lib/Widgets/prescription_item_tile.dart
+++ lib/Widgets/prescription_item_tile.dart
@@ -68,6 +68,7 @@ class PrescriptionItemTile extends StatelessWidget {
               ),
             ),
             Expanded(
+              flex:2,
               child: Container(
                 margin: EdgeInsets.symmetric(vertical: 8.0),
                 child: MaterialButton(
@@ -97,32 +98,30 @@ class PrescriptionItemTile extends StatelessWidget {
                 ),
               ),
             ),
-            Container(
-              width: 85,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text("Total",
-                      style: GoogleFonts.montserrat(
-                          fontSize: 14.0, color: Colors.black54)),
-                  SizedBox(height: 2.0),
-                  Text(getPrice(price),
-                      style: GoogleFonts.montserrat(
-                          fontSize: 16.0,
-                          color: getPriceColor(price),
-                          fontWeight: FontWeight.w500)),
-                  Text("PKR",
-                      style: GoogleFonts.montserrat(
-                        fontSize: 14.0,
-                        color: Colors.black54,
-                      )),
-                ],
-              ),
-              decoration: BoxDecoration(
-                border: Border(
-                  left: BorderSide(
-                      color: Colors.grey[300], style: BorderStyle.solid),
+
+            VerticalDivider(endIndent: 12,indent: 12,color: Colors.black54,thickness: 0,width: 0,),
+            Expanded(flex:1,
+              child: Container(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text("Total",
+                        style: GoogleFonts.montserrat(
+                            fontSize: 14.0, color: Colors.black54)),
+                    SizedBox(height: 2.0),
+                    Text(getPrice(price),
+                        style: GoogleFonts.montserrat(
+                            fontSize: 14.0,
+                            color: getPriceColor(price),
+                            fontWeight: FontWeight.w500)),
+                    Text("PKR",
+                        style: GoogleFonts.montserrat(
+                          fontSize: 14.0,
+                          color: Colors.black54,
+                        )),
+                  ],
                 ),
+
               ),
             ),
           ],
